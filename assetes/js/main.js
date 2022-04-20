6a0814572ad4693330960891bda4861934c1b539
shamim <shaamim.hossaain@gmail.com>
2022-04-20 16:48:46 +0600

    html css javascrpt updated

---
+++ assetes/js/main.js
@@ -0,0 +1,22 @@
+
+
+document.getElementById("bolod-items").style.display= "none";
+document.getElementById("kobold-item-option").style.display= "none";
+function item(){
+    
+    if(document.getElementById('kobold').click){
+        document.getElementById("kobold-item-option").style.display="block";
+        
+    }
+}
+
+
+function vacumm(){
+    if(document.getElementById('vacum-cleaner').click){
+        document.getElementById("bolod-items").style.display="block";
+        document.getElementById('repair').style.display="none";
+        
+    }
+   
+    
+}
